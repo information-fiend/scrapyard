7d885e91adec79b6a9e65b5e36e8269b531cbea4
mattsoh <mattsoh100@gmail.com>
2025-03-07 19:24:50 +0000

    update

--- pages/cambridge.js
+++ pages/cambridge.js
@@ -618,10 +618,9 @@ export default function ExampleCity() {
             ),
             'What do I need?': (
               <>
-                Your laptop, chargers, and an open mind! If you're going to an
-                overnight event, bring toiletries and sleeping bagstoo.
+                Your laptop, chargers, and an open mind!
                 Additionally, if you plan to work on a hardware project, bring
-                the tools you'll need.
+                the tools you'll need (except soldering irons and hot glue guns, which we'll provide).
               </>
             ),
             'I’m not good at coding. Can I still participate?': (
